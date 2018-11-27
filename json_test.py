f4da14e8cb984a60f759340108ebf262ecc7afbb
Duxianyou <40175851+Duxianyou@users.noreply.github.com>
2018-11-27 21:07:47 +0800

    去掉注释

--- json_test.py
+++ json_test.py
@@ -11,10 +11,10 @@ import json
 #     numbers_list = json.load(file_obj)
 # print(numbers_list)
 
-# user_name = input("Pleas input your name: ")
-# file_path = "json_test.json"
-# with open(file_path, 'w') as file_obj:
-#     json.dump(user_name, file_obj)
+user_name = input("Pleas input your name: ")
+file_path = "json_test.json"
+with open(file_path, 'w') as file_obj:
+    json.dump(user_name, file_obj)
 
 with open(file_path) as file_obj:
     user_name = json.load(file_obj)
